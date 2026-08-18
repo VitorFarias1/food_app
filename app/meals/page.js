b1cb82d44026f1eefd7ef931b789544c1e2184fc
VitorFarias1 <vitor.rodriguesfarias@gmail.com>
2026-08-18 08:19:51 -0300

    wip: simulate a db with sqlite

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,19 @@
 import Link from 'next/link'
 import classes from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals'
+import { Suspense } from 'react';
+
+async function Meals() {
+    const meals = await getMeals();
+
+    return <MealsGrid meals={meals} />
+}
 
 export default function MealsPage() {
-    return <>
+    
+    return (
+        <>
         <header className={classes.header}>
             <h1>Delicious meals, created{' '}
                 <span className={classes.highlight}>by you</span>
@@ -15,8 +25,11 @@ export default function MealsPage() {
                 </Link>
             </p>
         </header>
-        <main className={classes.main}>
-            <MealsGrid meals={[]} />
+            <main className={classes.main}>
+                <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>}>
+                    <Meals />
+                </Suspense>
         </main>
-    </>
+        </>
+    )
 }
