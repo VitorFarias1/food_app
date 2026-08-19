812cd317bc18a7f0b7c863fead2a131e0a7c6ca5
VitorFarias1 <vitor.rodriguesfarias@gmail.com>
2026-08-19 07:02:48 -0300

    wip: error component started

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>An error ocurred!</h1>
+            <p>Error fetching meals. Please try again later.</p>
+        </main>
+    )
+}
